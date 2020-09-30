77b5bfdd572d0b7e76621c102743a0584ead1262
pavini11 <56035994+pavini11@users.noreply.github.com>
2020-09-30 16:31:56 +0530

    Add files via upload

---
+++ abcd.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+//find first occurrence of a character in a given string
+int main()
+{
+    char a=20;
+    if(a==20){
+        cout<<"the number is: "<<a;
+    }
+}
